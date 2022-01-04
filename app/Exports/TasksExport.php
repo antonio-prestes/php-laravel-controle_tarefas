62f8576daa0f00db5eb73994a687c688365c55ea
Diego <diegoantoniodasp@hotmail.com>
2022-01-04 10:47:26 -0300

    238. Definindo títulos na exportação

--- app/Exports/TasksExport.php
+++ app/Exports/TasksExport.php
@@ -4,14 +4,28 @@ namespace App\Exports;
 
 use App\Models\Task;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TasksExport implements FromCollection
+class TasksExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return auth()->user()->tasks()->get();
+    }
+
+    public function headings(): array
+    {
+        // TODO: Implement headings() method.
+        return [
+            'ID da Tarefa',
+            'ID do usuário',
+            'Tarefa',
+            'Data limite',
+            'Data criação',
+            'Data atualização'
+        ];
     }
 }
